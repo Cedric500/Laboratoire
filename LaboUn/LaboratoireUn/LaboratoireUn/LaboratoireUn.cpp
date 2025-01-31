b6fd69eade5aa8d664ecca27314dada54cba9b24
Cedric Hayfield <1730001@collegebart.ca>
2025-01-31 11:32:38 -0500

    First commit

---
+++ LaboUn/LaboratoireUn/LaboratoireUn/LaboratoireUn.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+using namespace std;
+
+void Exercice1()
+{
+	int a = 1;
+	int b = 3;
+	int c = 7;
+	int d = 8;
+
+	int addition = a + b + c + d;
+	cout<< "La somme des nombres est :" << addition << endl;
+
+	float moyenne = addition / static_cast<float>(4);
+	cout<<  "La moyenne des nombres est :" << moyenne << endl;
+
+	int produit = a * b * c * d;
+	cout<<  "Le produit des nombres est :" << produit << endl;
+
+	// int max = ;
+	//cout<<  "Le nombre minimum est :" << max << endl;
+
+	// int min = ;
+	//cout<<  "Le nombre maximum est :" << min << endl;
+
+}
+
+void Exercice2()
+{
+
+}
+
+void Exercice3()
+{
+
+}
+
+void Exercice4()
+{
+
+}
+void Exercice5()
+{
+
+
+}
+
+void Exercice6()
+{
+
+}
+
+void Exercice7()
+{
+
+}
+
+void Exercice8()
+{
+
+}
+
+void Exercice9()
+{
+
+}
+
+void Exercice10()
+{
+
+}
+
+int main(int argc, char** argv)
+{
+	Exercice1();
+
+	return 0;
+}
