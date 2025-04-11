8817746a2b972cb0986d7e014d50297886b52133
DESKTOP-ND0SB6V\PC <cedrichayfield@gmail.com>
2025-04-10 21:29:43 -0400

    avancement labo deux

--- LaboDeux/LaboratoireDeux/LaboDeux/LaboDeux/LaboDeux.cpp
+++ LaboDeux/LaboratoireDeux/LaboDeux/LaboDeux/LaboDeux.cpp
@@ -1,20 +1,71 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 void ExerciceUn()
 {
-	
+	string word = "allo";
+
+	cin >> word;
+	cout << "Resultat :";
+
+	for (int i = 0; i < size(word); i++)
+	{
+		char letter = word.at(i);
+		letter++;
+		cout << letter;
+	}
 }
 
 void ExerciceDeux()
 {
+	string words = "hello world";
 
+	getline(cin, words);
+	cout << "Correction :";
+
+	for (int i = 0; i < size(words); i++)
+	{
+		char letter = words[i];
+
+		if (i == 0)
+		{
+			letter -= 32;
+		}
+		else if (words[i - 1] == 32)
+		{
+			letter -= 32;
+		}
+		words[i] = letter;
+	}
+	cout << words;
 }
 
 void ExerciceTrois()
 {
+	string words = "";
 
+	getline(cin, words);
+	cout << "Plus long mot :";
+
+	string longestWord = "";
+	string currentWord = "";
+
+	for (int i = 0; i < size(words); i++)
+	{
+		if (words[i] == 32)
+		{
+			if (size(currentWord) > size(longestWord))
+			{
+				longestWord = currentWord;
+			}
+			currentWord = "";
+			continue;
+		}
+		currentWord.push_back(words[i]);
+	}
+	cout << longestWord;
 }
 
 void ExerciceQuatre()
@@ -34,5 +85,13 @@ void ExerciceSix()
 
 int main()
 {
+	//Termine
+	//ExerciceUn();
+	//ExerciceDeux();
+	ExerciceTrois();
 
+	//NonTermine
+	//ExerciceQuatre();
+	//ExerciceCinq();
+	//ExerciceSix();
 }
